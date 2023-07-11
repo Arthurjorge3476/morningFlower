5cc474ce07de531a9e1ccd13894a497f9570e0bc
arthur da silva jorge <arthursilvajoge347@gmailcom>
2023-07-11 11:50:38 -0300

    banco de dados

--- morningflower_funcionarios.sql
+++ morningflower_funcionarios.sql
@@ -1,29 +1,11 @@
-CREATE DATABASE  IF NOT EXISTS `morningflower` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `morningflower`;
 USE `morningflower`;
--- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
---
--- Host: localhost    Database: morningflower
--- ------------------------------------------------------
--- Server version	8.0.33
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `funcionarios`
---
+
 
 DROP TABLE IF EXISTS `funcionarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `funcionarios` (
   `codigo` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(225) NOT NULL,
@@ -45,25 +27,33 @@ CREATE TABLE `funcionarios` (
   UNIQUE KEY `ctps_UNIQUE` (`ctps`),
   UNIQUE KEY `senha_UNIQUE` (`senha`),
   UNIQUE KEY `grupo_de_acesso_UNIQUE` (`grupo_de_acesso`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `funcionarios`
---
+CREATE TABLE `fornecedores` (
+ 
+  `nome` varchar(225) NOT NULL,
+  `sorenome` varchar(225) NOT NULL,
+  `cidade` varchar(25) NOT NULL,
+  `estado` varchar(25) NOT NULL,
+  `empresa` varchar(20) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `telefone` varchar(100) NOT NULL,
+  `cpf` char(8) NOT NULL,
+  UNIQUE KEY `cpf_UNIQUE` (`cpf`)
+);
 
-LOCK TABLES `funcionarios` WRITE;
-/*!40000 ALTER TABLE `funcionarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `funcionarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+CREATE TABLE `produtos` (
+  `codigo`  varchar(225) NOT NULL,
+  `produto` varchar(225) NOT NULL,
+  `quantidade` varchar(10) NOT NULL,
+  `preco de compra` varchar(14) NOT NULL,
+  `preco de venda` varchar(20) NOT NULL,
+  `fornecedor` varchar(225) NOT NULL,
+  `validade` varchar(100) NOT NULL,
+  `obs do produto` varchar(225) NOT NULL,
+  UNIQUE KEY `codigo_UNIQUE` (`codigo`)
+  );
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-10 13:32:20
+
+
