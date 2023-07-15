38d33a90bcbdf2d2162e1dcd8e0b799548ddbd30
arthur da silva jorge <arthursilvajoge347@gmailcom>
2023-07-14 21:50:11 -0300

    bloco de notas

--- painel/home.php
+++ painel/home.php
@@ -1,37 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Morning Flower</title>
-    <link rel="stylesheet" href="../css/telainicial.css" />
-  </head>
-
-  <body>
-    <header>
-  
-      <nav>
-        <div class="logo"> <img src="../img/logo.png.png"> </div>
-      
-        <div class="mobile-menu">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
-        </div>
-        <ul class="nav-list">
-          <li><a href="home.php">Home</a></li>
-          <li><a href="">Presentes</a></li>
-          <li><a href="funcionarios.php">Funcionários</a></li>
-          <li><a href="produtos.php">Produtos</a></li>
-          <li><a href="fornecedores.php">Fornecedores</a></li>
-        </ul>
-      </nav>
-    </header>
-    <div class="area-login">
-
-    </div>
-    <?php
+<?php
 $filename = 'notepad.txt'; // Nome do arquivo de anotações
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -66,46 +33,48 @@ if ($content !== '') {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-    <title>Bloco de Anotações</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Morning Flower</title>
+    <link rel="stylesheet" href="../css/telainicial.css" />
+    
     <style>
-        body {
-            background-size: cover;
-            font-family: Arial, sans-serif;
-
-        }
-
         #notepad {
-            background-image: url('../img/folha_de_caderno.avif');
+            background-image:url('../img/folha_de_caderno.avif');
             background-repeat: no-repeat;
             width: 355px;
             height: 320px;
             padding: 35px;
             display: flex;
             flex-direction: column;
-            border-radius: 10px;
+            border-radius: 20px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             background-size: cover;
             font-family: 'Helvetica', sans-serif;
             justify-content: center;
             align-items: center;
-            margin: 0;
+            margin: 20px;
+            line-height: 1.0;
+            overflow: auto;
         }
 
         ul {
-            list-style-type: none;
-            padding-left: 0;
+            padding-left:5px;
+            list-style-type: number;
+            border-radius: 10px;
         }
 
         li {
             margin-bottom: 25px;
-            padding: 8px 16px;
-            border-radius: 4px;
+            padding: 8px 20px;
+            border-radius: 10px;
             cursor: pointer;
-            border: none;
-
+            border: 1px solid #ccc;
         }
+
         h1 {
             width: 300px;
             height: 10px;
@@ -118,32 +87,68 @@ if ($content !== '') {
         }
 
         button {
-        padding: 8px 16px;
-        background-color: #4caf50;
-        color: #fff;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
+            padding: 8px 16px;
+            background-color: #4caf50;
+            color: #fff;
+            border: none;
+            border-radius: 20px;
+            cursor: pointer;
         }
 
         button:hover {
-        background-color: #45a049;
+            background-color: #45a049;
         }
 
         .add-note-btn {
-        display: flex;
-        align-items: center;
+            display: flex;
+            align-items: center;
         }
 
         .add-note-btn i {
-        margin-right: 4px;
+            margin-right: 4px;
         }
 
+        .delete-note-btn {
+            background-color: #ff0000;
+            color: #fff;
+            border: none;
+            border-radius: 50%;
+            padding: 2px 6px;
+            font-size: 14px;
+            cursor: pointer;
+            margin-left: 4px;
+        }
 
+        .hidden {
+            display: none;
+        }
     </style>
 </head>
 <body>
-    <div id="notepad" >
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="../img/logo.png.png">
+            </div>
+      
+            <div class="mobile-menu">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-list">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="">Presentes</a></li>
+                <li><a href="funcionarios.php">Funcionários</a></li>
+                <li><a href="produtos.php">Produtos</a></li>
+                <li><a href="fornecedores.php">Fornecedores</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="area-login"></div>
+
+    <div id="notepad">
         <h1>Bloco de Anotações</h1>
         <form method="POST">
             <ul id="notes-list">
@@ -151,11 +156,15 @@ if ($content !== '') {
                     <li>
                         <input type="checkbox" name="notes[<?php echo $index; ?>][checked]" <?php echo $note['checked'] ? 'checked' : ''; ?>>
                         <input type="text" name="notes[<?php echo $index; ?>][content]" value="<?php echo $note['content']; ?>">
+                        <button type="button" class="delete-note-btn" onclick="deleteNote(this)">X</button>
                     </li>
                 <?php endforeach; ?>
             </ul>
-            <button type="button" onclick="addNote()">Adicionar Anotação</button>
-            <button type="submit">Salvar</button>
+            <div class="add-note-btn">
+                <button type="button" onclick="addNote()"><i>+</i>Adicionar Anotação</button>
+                <button type="submit" name="save-btn">Salvar</button>
+            </div>
+            
         </form>
     </div>
 
@@ -163,14 +172,14 @@ if ($content !== '') {
         function addNote() {
             var notesList = document.getElementById('notes-list');
             var li = document.createElement('li');
-            li.innerHTML = '<input type="checkbox" name="notes[][checked]"><input type="text" name="notes[][content]">';
+            li.innerHTML = '<input type="checkbox" name="notes[checked][]" value="1"><input type="text" name="notes[content][]" value=""><button type="button" class="delete-note-btn" onclick="deleteNote(this)">X</button>';
             notesList.appendChild(li);
+        }
+
+        function deleteNote(button) {
+            var li = button.parentNode;
+            li.parentNode.removeChild(li);
         }
     </script>
 </body>
 </html>
-
-
-
-
-
