cf32c8b7a330c18f6202c3a47c1aa32d7d9ae658
raissafraga <fragagoulartraissa20@gmail.com>
2023-08-29 23:53:03 -0300

    aplicando editar em fornecedores

--- painel/fornecedores.php
+++ painel/fornecedores.php
@@ -12,6 +12,66 @@ if (isset($_GET['excluir'])) {
 }
 
 
+if (isset($_POST['btnEditar'])) {
+  // Certifique-se de definir $id com o ID do funcionário que está sendo editado
+  $id = $_POST['idFornecedoresEditar'];
+
+  $conexao = conectar();
+
+  $codigo = $_POST['codigo'];
+  $nome = $_POST['nome'];
+  $endereco = $_POST['endereco'];
+  $cidade = $_POST['cidade'];
+  $bairro = $_POST['bairro'];
+  $estado = $_POST['estado'];
+  $cep = $_POST['cep'];
+  $telefone1 = $_POST['telefone1'];
+  $telefone2 = $_POST['telefone2'];
+  $email = $_POST['email'];
+  $cnpj = $_POST['cnpj'];
+  $vendedor = $_POST['vendedor'];
+  $telefonevendedor1 = $_POST['telefonevendedor1'];
+  $telefonevendedor2 = $_POST['telefonevendedor2'];
+  $condicaodavenda = $_POST['condicaodavenda'];
+  $atividade = $_POST['atividade'];
+
+  // Atualize o funcionário no banco de dados
+  $sql_update = "UPDATE fornecedores SET codigo = :codigo, nome = :nome, endereco = :endereco, cidade = :cidade, bairro = :bairro, estado = :estado, cep = :cep, telefone1 = :telefone1, telefone2 = :telefone2, email = :email, cnpj = :cnpj, vendedor = :vendedor,telefonevendedor1 = :telefonevendedor1,telefonevendedor2 = :telefonevendedor2,condicaodavenda = :condicaodavenda,atividade = :atividade WHERE id = :id";
+
+  $stmt_update = $conexao->prepare($sql_update);
+
+  $stmt_update->bindParam(':id', $id);
+  $stmt_update->bindParam(':codigo', $codigo);
+  $stmt_update->bindParam(':nome', $nome);
+  $stmt_update->bindParam(':endereco', $endereco);
+  $stmt_update->bindParam(':cidade', $cidade);
+  $stmt_update->bindParam(':bairro', $bairro);
+  $stmt_update->bindParam(':estado', $estado);
+  $stmt_update->bindParam(':cep', $cep);
+  $stmt_update->bindParam(':telefone1', $telefone1);
+  $stmt_update->bindParam(':telefone2', $telefone2);
+  $stmt_update->bindParam(':email', $email);
+  $stmt_update->bindParam(':cnpj', $cnpj);
+  $stmt_update->bindParam(':vendedor', $vendedor);
+  $stmt_update->bindParam(':telefonevendedor1', $telefonevendedor1);
+  $stmt_update->bindParam(':telefonevendedor2', $telefonevendedor2);
+  $stmt_update->bindParam(':condicaodavenda', $condicaodavenda);
+  $stmt_update->bindParam(':atividade', $atividade);
+
+  $result_update = $stmt_update->execute();
+
+  if (!$result_update) {
+      var_dump($stmt_update->errorInfo());
+      exit;
+  } else {
+      echo $stmt_update->rowCount() . " Linha atualizada";
+      header("Location: index.php?acao=fornecedores");
+      exit;
+  }
+}
+
+
+
 ?>
  <div class="search-container">
  <div class="search-wrapper">
@@ -46,7 +106,151 @@ if (isset($_GET['excluir'])) {
         <td><?php echo $linha['email'];?></td>
         <td><?php echo $linha['telefone1'];?></td>
         <td>
-          <button type="submit" class="btn btn-cadastro">editar</button>
+        <div>
+        <a href="index.php?acao=fornecedores&editar=<?php echo $linha['id']; ?>" class="btn btn-outline-warning btn-sm editar-fornecedores" data-info='<?php echo json_encode($linha); ?>' data-toggle="modal" data-target="#editarFornecedoresModal" name="editar">Editar</a>
+
+        <div class="modal fade " id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Editar Fornecedor</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <form id="formEditarFornecedor" method="POST" action="index.php?acao=fornecedores">
+          <!-- Campos de edição de funcionário -->
+          <input type="hidden" id="idFornecedoresEditar" name="idFornecedoresEditar">
+            <div class="form-row">
+              <div class="form-group col-md-2">
+                <label for="codigodofornecedor">Código*</label>
+                <input type="text" class="form-control" id="codigodofornecedor" name="codigo" required>
+              </div>
+
+              <div class="form-group col-md-12">
+                <label for="nomedofornecedor">Nome da Empresa*</label>
+                <input type="text" class="form-control" id="nomedofornecedor" name="nome" required>
+              </div>
+
+              <div class="form-group col-md-6">
+                  <label for="enderecodofornecedor">Endereço</label>
+                  <input type="texto" class="form-control" id="enderecodofornecedor" name="endereco">
+                </div>
+
+               <div class="form-group col-md-6">
+                  <label for="cidadedofornecedor">Cidade</label>
+                  <input type="texto" class="form-control" id="cidadedofornecedor" name="cidade">
+                </div>
+
+                <div class="form-group col-md-2">
+                  <label for="bairrodofornecedor">Bairro</label>
+                  <input type="texto" class="form-control" id="bairrodofornecedor" name="bairro">
+                </div>
+
+               <div class="form-group col-md-2" >
+                  <label for="estadodofornecedor" >Estado</label>
+                  <select class="form-control" aria-label=".form-select-lg example" id="estadodofornecedor" name="estado" style="border-radius: 18px;font-size: 19px; ">
+                    <option selected> </option>
+                    <option value="AC">AC</option>
+                    <option value="AL">AL</option>
+                    <option value="AP">AP</option>
+                    <option value="AM">AM</option>
+                    <option value="BA">BA</option>
+                    <option value="CE">CE</option>
+                    <option value="DF">DF</option>
+                    <option value="ES">ES</option>
+                    <option value="CO">CO</option>
+                    <option value="MA">MA</option>
+                    <option value="MT">MT</option>
+                    <option value="MS">MS</option>
+                    <option value="MG">MG</option>
+                    <option value="PA">PA</option>
+                    <option value="PB">PB</option>
+                    <option value="PR">PR</option>
+                    <option value="PE">PE</option>
+                    <option value="PI">PI</option>
+                    <option value="RJ">RJ</option>
+                    <option value="RN">RN</option>
+                    <option value="RS">RS</option>
+                    <option value="RO">RO</option>
+                    <option value="RR">RR</option>
+                    <option value="SC">SC</option>
+                    <option value="SP">SP</option>
+                    <option value="SE">SE</option>
+                    <option value="TO">TO</option>
+
+
+                  </select>
+                </div>
+
+                <div class="form-group col-md-2">
+                  <label for="cepdofornecedor">CEP</label>
+                  <input type="texto" class="form-control" id="cepdofornecedor" name="cep" oninput="this.value = formatarCEP(this.value);" maxlength="9">
+                </div>
+
+                <div class="form-group col-md-3">
+                  <label for="telefone1dofornecedor">Telefone 1*</label>
+                  <input type="texto" class="form-control" id="telefone1dofornecedor" name="telefone1" oninput="this.value = formatarTelefone(this.value);" maxlength="15" required>
+                </div>
+
+                <div class="form-group col-md-3">
+                  <label for="telefone2dofornecedor">Telefone 2</label>
+                  <input type="texto" class="form-control" id="telefone2dofornecedor" name="telefone2" oninput="this.value = formatarTelefone2(this.value);" maxlength="15">
+                </div>
+
+              <div class="form-group col-md-6">
+                <label for="emaildofornecedor">Email*</label>
+                <input type="email" class="form-control" id="emaildofornecedor" name="email">
+              </div>
+
+              <div class="form-group col-md-6">
+                <label for="cnpjdofornecedor">CNPJ*</label>
+                <input type="text" class="form-control" id="cnpjdofornecedor" name="cnpj"  oninput="this.value = formatarCNPJ(this.value);" maxlength="18" required>
+              </div>
+
+              <div class="form-group col-md-6">
+                <label for="vendedordofornecedor">Vendedor</label>
+                <input type="text" class="form-control" id="vendedordofornecedor" name="vendedor">
+              </div>
+
+              <div class="form-group col-md-3">
+                <label for="telefonedofornecedor">Telefone 1*</label>
+                <input type="text" class="form-control" id="telefonedofornecedor" name="telefonevendedor1" oninput="this.value = formatarTelefonevendedor(this.value);" maxlength="15">
+              </div>
+
+              <div class="form-group col-md-3">
+                <label for="telefonedovendedor">Telefone 2</label>
+                <input type="text" class="form-control" id="telefonedovendedor" name="telefonevendedor2"  oninput="this.value = formatarTelefonevendedor2(this.value);" maxlength="15">
+              </div>
+              
+              <div class="form-group col-md-6">
+                <label for="codicaodavenda">Condição da venda</label>
+                <input type="text" class="form-control" id="codicaodavenda" name="condicaodavenda">
+              </div>
+
+
+              <div class="form-group col-md-6">
+                <label for="atividadedavenda">Atividade</label>
+                <input type="text" class="form-control" id="atividadedavenda" name="atividade">
+              </div>
+
+
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <input type="submit" for="formEditarFornecedores" name="btnEditar" class="btn btn-primary">
+
+              </div>
+          </form>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+
+        </div>
+
         </td>
         <td>
         <a href="index.php?acao=fornecedores&excluir" class="btn btn-outline-danger btn-sm excluir-fornecedores" data-id="<?php echo $linha['id']; ?>" data-nome="<?php echo $linha['nome']; ?>" >excluir</a>
@@ -294,4 +498,64 @@ if (isset($_GET['excluir'])) {
       });
     });
   });
+</script>
+
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const editarLinks = document.querySelectorAll('.editar-fornecedores');
+        const idFornecedoresEditar = document.getElementById('idFornecedoresEditar');
+        const codigodofornecedor = document.getElementById('codigodofornecedor');
+        const nomedofornecedor = document.getElementById('nomedofornecedor'); // Campo CPF
+        const enderecodofornecedor = document.getElementById('enderecodofornecedor');
+        const cidadedofornecedor = document.getElementById('cidadedofornecedor');
+        const bairrodofornecedor = document.getElementById('bairrodofornecedor');
+        const estadodofornecedor = document.getElementById('estadodofornecedor');
+        const cepdofornecedor = document.getElementById('cepdofornecedor');
+        const telefone1dofornecedor = document.getElementById('telefone1dofornecedor');
+        const telefone2dofornecedor = document.getElementById('telefone2dofornecedor');
+        const emaildofornecedor = document.getElementById('emaildofornecedor');
+        const cnpjdofornecedor = document.getElementById('cnpjdofornecedor');
+        const vendedordofornecedor = document.getElementById('vendedordofornecedor');
+        const telefonedofornecedor = document.getElementById('telefonedofornecedor');
+        const telefonedovendedor = document.getElementById('telefonedovendedor');
+        const codicaodavenda = document.getElementById('codicaodavenda');
+        const atividadedavenda = document.getElementById('atividadedavenda');
+        // Outros campos de edição...
+
+        editarLinks.forEach(function (link) {
+            link.addEventListener('click', function (event) {
+                event.preventDefault();
+
+                // Obtenha as informações do funcionário a partir do atributo 'data-info'
+                const infoFuncionario = JSON.parse(this.getAttribute('data-info'));
+
+                // Preencha os campos da modal com as informações do funcionário
+                idFornecedoresEditar.value = infoFornecedor.id;
+                codigodofornecedor.value = infoFuncionario.codigo;
+                nomedofornecedor.value = infoFuncionario.cpf;
+                enderecodofornecedor.value = infoFuncionario.data_de_nascimento; 
+                cidadedofornecedor.value = infoFuncionario.rg;
+                bairrodofornecedor.value = infoFuncionario.ctps;
+                estadodofornecedor.value = infoFuncionario.cidade;
+                cepdofornecedor.value = infoFuncionario.endereco;
+                telefone1dofornecedor.value = infoFuncionario.cep;
+                telefone2dofornecedor.value = infoFuncionario.email;
+                emaildofornecedor.value = infoFuncionario.telefone;
+                cnpjdofornecedor.value = infoFuncionario.senha;
+                vendedordofornecedor.value = infoFuncionario.grupo_de_acesso;
+                telefonedofornecedor.value = infoFuncionario.grupo_de_acesso;
+                telefonedovendedor.value = infoFuncionario.grupo_de_acesso;
+                codicaodavenda.value = infoFuncionario.grupo_de_acesso;
+                atividadedavenda.value = infoFuncionario.grupo_de_acesso;
+                // Preencha outros campos de edição conforme necessário
+
+                // Abra a modal de edição
+                $('#editarFornecedoresModal').modal('show');
+            });
+        });
+
+        // Resto do seu código JavaScript...
+    });
 </script>
