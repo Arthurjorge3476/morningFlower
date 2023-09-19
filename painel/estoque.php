ff1d0564213c0751d6ff2fbcdfe175d8fdda4387
raissafraga <fragagoulartraissa20@gmail.com>
2023-09-18 22:59:38 -0300

    aviso de que o estoque está baixo

--- painel/estoque.php
+++ painel/estoque.php
@@ -1,16 +1,10 @@
 <?php
-
 $listaProdutos = select('produtos');
-
-
-
 
 if (isset($_POST['pesquisar'])) {
     $termoPesquisa = $_POST['pesquisar'];
-    $listaProdutos = array_filter($listaProdutos, function ($produtos) use ($termoPesquisa) {
-        // Aqui, você pode ajustar como deseja que a pesquisa seja realizada.
-        // Atualmente, ela é case-insensitive e verifica se o nome contém o termo de pesquisa.
-        return stripos($produtos['produto'], $termoPesquisa) !== false;
+    $listaProdutos = array_filter($listaProdutos, function ($produto) use ($termoPesquisa) {
+        return stripos($produto['produto'], $termoPesquisa) !== false;
     });
 }
 ?>
@@ -26,13 +20,15 @@ if (isset($_POST['pesquisar'])) {
             </button>
         </div>
     </div>
-</form>
+    <div id="estoqueBaixoAlert" class="alert alert-danger" style="display: none;">
+    Estoque Baixo! A quantidade em estoque está abaixo do mínimo.
+</div>
 
+</form>
 
 <div class="tabela">
     <table class="table table-bordered table-hover mt-3">
         <thead class="table-dark cordatabela">
-
             <tr>
                 <th scope="col"></th>
                 <th scope="col">Código</th>
@@ -41,28 +37,50 @@ if (isset($_POST['pesquisar'])) {
                 <th scope="col">Quantidade</th>
                 <th scope="col">Preço de Venda</th>
                 <th scope="col">Imagem do Produto</th>
-
-
             </tr>
         </thead>
         <tbody>
             <?php
-            foreach ($listaProdutos as $indice => $linha) { ?>
-
-                <tr class="">
+            foreach ($listaProdutos as $indice => $linha) { 
+                $quantidadeEmEstoque = $linha['estoque'];
+                $estoqueMinimo = 5;
+                $estoqueBaixo = $quantidadeEmEstoque <= $estoqueMinimo;
+            ?>
+                <tr class="<?php echo $estoqueBaixo ? 'estoque-baixo' : ''; ?>">
                     <th scope="row"><?php echo $indice + 1; ?></th>
                     <td><?php echo $linha['codigo']; ?></td>
                     <td><?php echo $linha['produto']; ?></td>
                     <td><?php echo $linha['validade']; ?></td>
                     <td><?php echo $linha['estoque']; ?></td>
                     <td><?php echo $linha['precodevenda']; ?></td>
-
                     <td>
                         <img src="<?php echo $linha['imagem']; ?>" width="75" height="75" />
                     </td>
                 </tr>
-            <?php };
-            ?>
-
+            <?php } ?>
         </tbody>
     </table>
+</div>
+
+<script>
+    <?php
+    $estoqueMinimo = 5; // Defina o estoque mínimo desejado
+    $estoqueBaixoEncontrado = false; // Variável para rastrear se um produto com estoque baixo foi encontrado
+
+    // Verifica cada produto
+    foreach ($listaProdutos as $linha) { 
+        $quantidadeEmEstoque = $linha['estoque'];
+        if ($quantidadeEmEstoque <= $estoqueMinimo) {
+            $estoqueBaixoEncontrado = true; // Estoque baixo encontrado
+            break; // Pare de verificar assim que encontrar um produto com estoque baixo
+        }
+    }
+    ?>
+
+    // Após o loop, use a variável $estoqueBaixoEncontrado para mostrar ou ocultar a mensagem de aviso
+    if (<?php echo $estoqueBaixoEncontrado ? 'true' : 'false'; ?>) {
+        document.getElementById('estoqueBaixoAlert').style.display = 'block';
+    }
+</script>
+
+
