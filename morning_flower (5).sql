0d8880848ac8cc75108ee1dad8d605291e977a11
arthur da silva jorge <arthursilvajoge347@gmailcom>
2023-09-21 18:14:26 -0300

    ada

--- morning_flower (5).sql
+++ morning_flower (5).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20/09/2023 às 03:41
+-- Tempo de geração: 21-Set-2023 às 23:12
 -- Versão do servidor: 10.4.27-MariaDB
--- Versão do PHP: 8.0.25
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `blocodenotas`
+-- Estrutura da tabela `blocodenotas`
 --
 
 CREATE TABLE `blocodenotas` (
@@ -34,7 +34,7 @@ CREATE TABLE `blocodenotas` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `clientes`
+-- Estrutura da tabela `clientes`
 --
 
 CREATE TABLE `clientes` (
@@ -49,16 +49,17 @@ CREATE TABLE `clientes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `clientes`
+-- Extraindo dados da tabela `clientes`
 --
 
 INSERT INTO `clientes` (`id`, `nome`, `cpf`, `endereco`, `telefone`, `cidade`, `cep`, `email`) VALUES
-(2, 'JOSÉ ADEMIR BARBOZA JORGE', '990.990.090.90', 'policia rodoviaria', '(48) 99159-0905', 'Araranguá / SC', 8890231, 'arthur@gmail.com');
+(2, 'JOSÉ ADEMIR BARBOZA JORGE', '990.990.090.90', 'policia rodoviaria', '(48) 99159-0905', 'Araranguá / SC', 8890231, 'arthur@gmail.com'),
+(4, 'ARTHUR DA SILVA JORGE', '377.331.170.20', 'aehjj', '(48) 99159-0905', 'Sombrio / SC', 99999, 'arthursilvajorge347@gmail.com');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `comprovante`
+-- Estrutura da tabela `comprovante`
 --
 
 CREATE TABLE `comprovante` (
@@ -68,16 +69,20 @@ CREATE TABLE `comprovante` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `comprovante`
+-- Extraindo dados da tabela `comprovante`
 --
 
 INSERT INTO `comprovante` (`id`, `pedido`, `quantidade`) VALUES
-(1, 8, 24);
+(4, 11, 5),
+(5, 12, 23),
+(6, 13, 4),
+(7, 14, 1),
+(8, 15, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `fornecedores`
+-- Estrutura da tabela `fornecedores`
 --
 
 CREATE TABLE `fornecedores` (
@@ -101,16 +106,16 @@ CREATE TABLE `fornecedores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `fornecedores`
+-- Extraindo dados da tabela `fornecedores`
 --
 
 INSERT INTO `fornecedores` (`id`, `codigo`, `nome`, `endereco`, `cidade`, `bairro`, `estado`, `cep`, `telefone1`, `telefone2`, `email`, `cnpj`, `vendedor`, `telefonevendedor1`, `telefonevendedor2`, `condicaodavenda`, `atividade`) VALUES
-(1, '', 'Vaquinha Mumu', 'Avenida Damasio Péres', 'Santa Rosa do Sul', 'centro', 'SC', '88965000', '48996518770', '', 'vaquinhamumu@gmail.com', '45698745812', 'Vanesa', '996588471', '', 'Doces', '');
+(1, '12121211', 'Vaquinha Mumú', 'Avenida Damasio Péres', 'Santa Rosa do Sul', 'centro', 'SC', '88965000', '48996518770', '', 'vaquinhamumu@gmail.com', '45698745812', 'Vanesa', '996588471', '', 'Doces', '');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `funcionarios`
+-- Estrutura da tabela `funcionarios`
 --
 
 CREATE TABLE `funcionarios` (
@@ -130,18 +135,18 @@ CREATE TABLE `funcionarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `funcionarios`
+-- Extraindo dados da tabela `funcionarios`
 --
 
 INSERT INTO `funcionarios` (`nome`, `data_de_nascimento`, `rg`, `cpf`, `ctps`, `cidade`, `endereco`, `cep`, `email`, `telefone`, `senha`, `grupo_de_acesso`, `id`) VALUES
-('Raquel', '2006-01-23', '69.852.155', '255.648.822-12', '555556598', 'Santa Rosa do Sul', 'Avenida Damasio Péres', '88965-000', 'admin@admin.com', '(48) 96588-4885', 'e78caf5693', '2', 52),
-('Sabrina', NULL, NULL, '523.654.885.22', NULL, NULL, NULL, NULL, 'sabrinafraga@gmail.com', '(48) 95654-8522', '808756e43b', '2', 84),
-('Raissa', NULL, NULL, '255.549.888.85', NULL, NULL, NULL, NULL, 'raissafraga@gmail.com', '(45) 88778-9451', 'e10adc3949ba59abbe56e057f20f883e', '1', 89);
+('thalia', '0000-00-00', '123.456.78', '523.654.885.22', 'zzcczcz', 'Torres', 'Vila', '1165382', 'thalia@gmail', '19938883', 'ffb091d89693c69bb5206e5c385f3bd9', '2', 84),
+('thalia', '0000-00-00', '413.154.11', '255.549.888.87', 'dadadad', 'Torres', 'Vila', '1165382', 'thalia@gmail', '19938883', 'f4cc399f0effd13c888e310ea2cf5399', '1', 89),
+('ARTHUR DA SILVA JORGE', NULL, NULL, '377.331.170.20', NULL, 'Sombrio / SC', 'aehjj', '99999-999', 'raissafraga@gmail.com', '(48) 99159-0905', 'e10adc3949ba59abbe56e057f20f883e', '1', 90);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `itens_pedido`
+-- Estrutura da tabela `itens_pedido`
 --
 
 CREATE TABLE `itens_pedido` (
@@ -153,15 +158,22 @@ CREATE TABLE `itens_pedido` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `itens_pedido`
+-- Extraindo dados da tabela `itens_pedido`
 --
 
 INSERT INTO `itens_pedido` (`id`, `pedido_id`, `produto`, `quantidade`, `preco_unitario`) VALUES
+(14, 11, 'rosas', 2, 3),
+(15, 11, 'girassol', 3, 1),
+(16, 12, 'rosas', 23, 1),
+(17, 13, 'rosas', 2, 3),
+(18, 13, 'girassol', 2, 3),
+(19, 14, ' flower', 1, 2),
+(20, 15, 'morning flower', 1, 3);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `pedido`
+-- Estrutura da tabela `pedido`
 --
 
 CREATE TABLE `pedido` (
@@ -172,17 +184,22 @@ CREATE TABLE `pedido` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `pedido`
+-- Extraindo dados da tabela `pedido`
 --
 
 INSERT INTO `pedido` (`id`, `data`, `cliente`, `valorTotal`) VALUES
-
-
+(11, '2023-09-20', 'ARTHUR DA SILVA JORGE', 9),
+(12, '2023-09-21', 'ARTHUR DA SILVA JORGE', 23),
+(13, '2023-09-21', 'ARTHUR DA SILVA JORGE', 12),
+(14, '2023-09-21', 'ARTHUR DA SILVA JORGE', 2),
+(15, '2023-09-21', 'ARTHUR DA SILVA JORGE', 3),
+(16, '2023-09-21', 'ARTHUR DA SILVA JORGE', 0),
+(17, '2023-09-21', 'ARTHUR DA SILVA JORGE', 0);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `produtos`
+-- Estrutura da tabela `produtos`
 --
 
 CREATE TABLE `produtos` (
@@ -196,43 +213,41 @@ CREATE TABLE `produtos` (
   `validade` varchar(100) NOT NULL,
   `observacao` varchar(225) DEFAULT NULL,
   `categoria` varchar(45) DEFAULT NULL,
-  `margemdelucro` varchar(45) NOT NULL,
-  `lucroanterior` varchar(45) NOT NULL,
   `imagem` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `produtos`
+-- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id`, `codigo`, `produto`, `estoque`, `precodecompra`, `precodevenda`, `fornecedor`, `validade`, `observacao`, `categoria`, `margemdelucro`, `lucroanterior`, `imagem`) VALUES
-(4, 8890231, 'girassol', '31', '11', '31', 'Vaquinha Mumu', '2023-09-28', '', 'planta', '13', '31', 'images.jpg'),
-(5, 488, 'Rosas Vermelhas', '50', '200,00', '15,00', 'fornecedor', '2000-08-31', NULL, 'planta', '10%', '5%', '../uploads/download.jpg');
+INSERT INTO `produtos` (`id`, `codigo`, `produto`, `estoque`, `precodecompra`, `precodevenda`, `fornecedor`, `validade`, `observacao`, `categoria`, `imagem`) VALUES
+(6, 99993, 'girassol', '13', '11', '21', 'Vaquinha Mumu', '2023-09-21', NULL, NULL, '../uploads/girassol.jpg'),
+(9, 99999, 'rosa', '3', '11', '3', 'Vaquinha Mumú', '2023-10-04', NULL, 'planta', '../uploads/rosas.jpg');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `blocodenotas`
+-- Índices para tabela `blocodenotas`
 --
 ALTER TABLE `blocodenotas`
   ADD PRIMARY KEY (`notas`);
 
 --
--- Índices de tabela `clientes`
+-- Índices para tabela `clientes`
 --
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `comprovante`
+-- Índices para tabela `comprovante`
 --
 ALTER TABLE `comprovante`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `fornecedores`
+-- Índices para tabela `fornecedores`
 --
 ALTER TABLE `fornecedores`
   ADD PRIMARY KEY (`id`,`cnpj`),
@@ -246,7 +261,7 @@ ALTER TABLE `fornecedores`
   ADD UNIQUE KEY `telefonevendedor2_UNIQUE` (`telefonevendedor2`);
 
 --
--- Índices de tabela `funcionarios`
+-- Índices para tabela `funcionarios`
 --
 ALTER TABLE `funcionarios`
   ADD PRIMARY KEY (`id`),
@@ -256,69 +271,69 @@ ALTER TABLE `funcionarios`
   ADD UNIQUE KEY `ctps_UNIQUE` (`ctps`);
 
 --
--- Índices de tabela `itens_pedido`
+-- Índices para tabela `itens_pedido`
 --
 ALTER TABLE `itens_pedido`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `pedido`
+-- Índices para tabela `pedido`
 --
 ALTER TABLE `pedido`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `produtos`
+-- Índices para tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `codigo_UNIQUE` (`codigo`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `comprovante`
 --
 ALTER TABLE `comprovante`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de tabela `fornecedores`
 --
 ALTER TABLE `fornecedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `funcionarios`
 --
 ALTER TABLE `funcionarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 
 --
 -- AUTO_INCREMENT de tabela `itens_pedido`
 --
 ALTER TABLE `itens_pedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de tabela `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
